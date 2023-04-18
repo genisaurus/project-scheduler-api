b8b3a5d93cb3a59f76b844be42699b67cc800c32
genisaurus <rusty.barron@gmail.com>
2023-04-18 14:43:42 -0400

    created model classes

---
+++ src/main/java/com/russell/scheduler/models/UserRole.java
@@ -0,0 +1,62 @@
+package com.russell.scheduler.models;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name="user_roles")
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "role_name", nullable = false, unique = true)
+    private String roleName;
+
+    public UserRole() {
+        super();
+    }
+
+    public UserRole(Integer id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserRole userRole = (UserRole) o;
+        return Objects.equals(id, userRole.id) && Objects.equals(roleName, userRole.roleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, roleName);
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole{" +
+                "id=" + id +
+                ", roleName='" + roleName + '\'' +
+                '}';
+    }
+}
